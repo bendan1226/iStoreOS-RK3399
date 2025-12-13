53d03dd86b31f6d477698215f21e52688b888612
Lemon <149338935+Lemon1151@users.noreply.github.com>
2025-12-13 10:45:54 +0800

    Update diy-part2.sh

--- dg3399/diy-part2.sh
+++ dg3399/diy-part2.sh
@@ -16,7 +16,8 @@ echo -e "\\ndefine Device/rockchip_dg3399
   SOC := rk3399
   UBOOT_DEVICE_NAME := dg3399-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-ata-ahci kmod-rtl8821ae kmod-usb-net-rtl8152
+  DEVICE_PACKAGES := kmod-ata-ahci kmod-rtl8821ae kmod-usb-net-rtl8152 wpad \
+    brcmfmac-nvram-43455-sdio cypress-firmware-43455-sdio
 endef
 TARGET_DEVICES += rockchip_dg3399" >> target/linux/rockchip/image/armv8.mk
 
