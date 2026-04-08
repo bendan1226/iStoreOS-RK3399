6883027f4fc281f7b9c10b1344a4882d484657d4
yizhanghong <865362414@qq.com>
2026-04-08 21:18:59 +0800

    Update diy-part2.sh

--- fnet3399/diy-part2.sh
+++ fnet3399/diy-part2.sh
@@ -36,6 +36,10 @@ cp -f $GITHUB_WORKSPACE/fnet3399/kernel-rockchip/02_network target/linux/rockchi
 cp -a $GITHUB_WORKSPACE/fnet3399/packages/* package/firmware/
 cp -f $GITHUB_WORKSPACE/fnet3399/opwifi package/base-files/files/etc/init.d/opwifi
 chmod 755 package/base-files/files/etc/init.d/opwifi
+echo "
+CONFIG_PACKAGE_brcmfmac-firmware-fnet3399=y
+" >> .config
+
 
 # 集成CPU性能跑分脚本
 cp -f $GITHUB_WORKSPACE/configfiles/coremark/coremark-arm64 package/base-files/files/bin/coremark-arm64
